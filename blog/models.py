3a46b55090b327c2860f7bd3d2c1140571ff6e10
Rush <aleksey@levadniy.ru>
2020-02-04 00:24:20 +0300

    Add posts, tags, comments

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,13 @@ class Tag(models.Model):
     name = models.CharField("Имя", max_length=100)
     slug = models.SlugField("url", max_length=100)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
 class Post(models.Model):
     """Модель постов"""
     title = models.CharField("Имя", max_length=100)
@@ -25,8 +32,22 @@ class Post(models.Model):
     created_date = models.DateTimeField()
     slug = models.SlugField("url", max_length=100)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
 class Comment(models.Model):
     """Модель комментариев"""
     text = models.TextField()
     created_date = models.DateTimeField()
     moderation = models.BooleanField()
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.base import View
 from .models import Category
+from .models import Post
+
 
 # def home(request):
 #     if request.method == "POST":
@@ -16,9 +18,12 @@ class HomeView(View):
         # def post(self, request):
         #     return HttpResponse("It's POST request")
         """Home page"""
+        # def get(self, request):
+        #     category_list = Category.objects.all()
+        #     return render(request, "blog/home.html", {"categories": category_list})
         def get(self, request):
-            category_list = Category.objects.all()
-            return render(request, "blog/home.html", {"categories": category_list})
+                post_list = Post.objects.all()
+                return render(request, "blog/home.html", {"posts": post_list})
 
 class CategoryView(View):
         def get(self, request, category_name):
